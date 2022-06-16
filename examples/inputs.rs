81415a1285521490d211417b1fff5e33f10f924d
Carlo Supina <cdsupina@gmail.com>
2022-06-16 16:23:24 -0500

    adjust input example for 0.7

--- examples/inputs.rs
+++ examples/inputs.rs
@@ -1,62 +1,30 @@
-use bevy::prelude::*;
-use rust_arcade_plugin::{ArcadeGamepad, RustArcadePlugin};
+use bevy::{
+    input::gamepad::{GamepadEvent, GamepadEventType},
+    prelude::*,
+};
 
 fn main() {
-    // Define window
-    let window = WindowDescriptor {
-        title: "Inputs".to_string(),
-        width: 1280.0,
-        height: 720.0,
-        resizable: false,
-        ..Default::default()
-    };
-
     App::new()
-        .insert_resource(window)
         .add_plugins(DefaultPlugins)
-        .add_plugin(RustArcadePlugin)
-        .add_system(gamepad_input_events)
+        .add_system(gamepad_events)
         .run();
 }
 
-fn gamepad_input_events(
-    my_gamepad: Option<Res<ArcadeGamepad>>,
-    mut gamepad_evr: EventReader<GamepadEvent>,
-) {
-    let gamepad = if let Some(gp) = my_gamepad {
-        // a gamepad is connected, we have the id
-        gp.0
-    } else {
-        // no gamepad is connected
-        return;
-    };
-
-    for GamepadEvent(id, kind) in gamepad_evr.iter() {
-        if id.0 != gamepad.0 {
-            // event not from our gamepad
-            continue;
+fn gamepad_events(mut gamepad_event: EventReader<GamepadEvent>) {
+    for event in gamepad_event.iter() {
+        match &event {
+            GamepadEvent(gamepad, GamepadEventType::Connected) => {
+                info!("{:?} Connected", gamepad);
+            }
+            GamepadEvent(gamepad, GamepadEventType::Disconnected) => {
+                info!("{:?} Disconnected", gamepad);
+            }
+            GamepadEvent(gamepad, GamepadEventType::ButtonChanged(button_type, value)) => {
+                info!("{:?} of {:?} is changed to {}", button_type, gamepad, value);
+            }
+            GamepadEvent(gamepad, GamepadEventType::AxisChanged(axis_type, value)) => {
+                info!("{:?} of {:?} is changed to {}", axis_type, gamepad, value);
+            }
         }
-
-        println!("{:?}", kind);
-
-        /*
-        use GamepadEventType::{AxisChanged, ButtonChanged};
-
-        match kind {
-            AxisChanged(GamepadAxisType::RightStickX, x) => {
-                // Right Stick moved (X)
-            }
-            AxisChanged(GamepadAxisType::RightStickY, y) => {
-                // Right Stick moved (Y)
-            }
-            ButtonChanged(GamepadButtonType::DPadDown, val) => {
-                // buttons are also reported as analog, so use a threshold
-                if *val > 0.5 {
-                    // button pressed
-                }
-            }
-            _ => {} // don't care about other inputs
-        }
-        */
     }
 }
